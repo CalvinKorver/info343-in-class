f7a11273954eed453ff4e7c7e1d87d871d529388
cjkorver <calvin.korver@gmail.com>
2016-10-31 09:51:01 -0700

    changes to master

--- firebase-auth/js/common.js
+++ firebase-auth/js/common.js
@@ -1,1 +1,15 @@
 //put your Firebase config and initialization code here
+
+
+
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCgmsV4bCs507CgZKzaoOVZuzZH4lQMUS8",
+    authDomain: "slick-4ee52.firebaseapp.com",
+    databaseURL: "https://slick-4ee52.firebaseio.com",
+    storageBucket: "slick-4ee52.appspot.com",
+    messagingSenderId: "551575730286"
+};
+firebase.initializeApp(config);
